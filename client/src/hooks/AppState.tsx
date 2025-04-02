b313c7e0e139ce6b791c3a9336d3eb5dfc96d3dd
Sam Keddy <skeddy13@yahoo.com>
2025-04-02 17:05:20 -0400

    created appstate

---
+++ client/src/hooks/AppState.tsx
@@ -0,0 +1,60 @@
+import { createContext, useContext, useReducer, FC, ReactNode } from 'react';
+
+type User = {
+	_id: string;
+	name: string;
+	email: string;
+	createdAt: Date;
+	updatedAt: Date;
+};
+
+type AppState = {
+	user: User | 'loading' | 'unregistered';
+};
+
+type SetUserAction = {type: 'SET_USER'; user: User};
+type Action = SetUserAction;
+
+const stateReducer = (state:AppState, action:Action) => {
+	if (action.type === 'SET_USER') {
+		return { ...state, user: action.user };
+	}
+	else {
+		throw new Error(`Unhandled action type: ${action.type}`);
+	}
+};
+
+type StateContextType = {
+	state: AppState;
+	dispatchState: React.Dispatch<Action>;
+};
+
+const StateContext = createContext<StateContextType>({
+	state: { user: 'loading' },
+	dispatchState: () => undefined,
+});
+
+export const AppStateProvider: FC<{ children: ReactNode }> = ({ children }) => {
+	const [appState, dispatchState] = useReducer(stateReducer, {user: 'loading'});
+
+	const contextValue = { 
+		state: appState,
+		dispatchState 
+	};
+
+	return (
+		<StateContext.Provider value={contextValue}>
+			{children}
+		</StateContext.Provider>
+	);
+};
+
+export const useAppState = () => {
+	const context = useContext(StateContext);
+
+	if (context === undefined) throw new Error('useAppState must be used within an StateProvider');
+	if (context.state.user === 'loading') throw new Error('User not loaded yet');
+	if (context.state.user === 'unregistered') throw new Error('User is unregistered');
+
+	return context;
+};
