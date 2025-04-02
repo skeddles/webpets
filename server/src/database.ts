5d07fea06660166cf9bdf9c8c4ddb6bc68b5fc47
Sam Keddy <skeddy13@yahoo.com>
2025-04-02 18:32:24 -0400

    database

---
+++ server/src/database.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { MongoClient, ServerApiVersion, Db, ObjectId } from 'mongodb';
+
+if (!process.env.MONGO_DB_CONNECTION_STRING) 
+	throw new Error('MONGO_DB_CONNECTION_STRING is not defined');
+
+const client = new MongoClient(process.env.MONGO_DB_CONNECTION_STRING, {
+	tlsCertificateKeyFile: '.mongo-cert.pem',
+	serverApi: ServerApiVersion.v1
+});
+
+await client.connect();
+	console.log('connecting to database...');
+const database:Db = client.db(process.env.MONGO_DB_DATABASE_NAME);
+	
+
+if (!database) {
+	console.error('failed to connect to database');
+	process.exit(1);
+} else console.log('connected to database');
+
+export function id (id:IdOrString) {
+	if (typeof id === 'string') return ObjectId.createFromHexString(id);
+	else return id as ObjectId;
+}
+
+export const Users = database.collection("users");
+
+await Users.createIndex({ "username": 1 },{ name:"username_lowercase_index", unique: true, collation: {locale: 'en',strength: 2}});
+
+export default database;
