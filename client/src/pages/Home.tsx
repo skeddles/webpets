ca1c3316e1161edfaff766c59a9d83f9343d7244
Sam Keddy <skeddy13@yahoo.com>
2025-04-08 19:08:00 -0400

    added home lesson list css

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,32 @@
 
 
+import './Home.css';
+
 interface HomeProps {
 
 }
 
 export default function Home({}: HomeProps) {
+
+	const lessonList = [
+		{ title: 'Lesson 1', slug: 'lesson-1' },
+		{ title: 'Lesson 2', slug: 'lesson-2' },
+		{ title: 'Lesson 3', slug: 'lesson-3' },
+	];
+
 	return (<div className="Home">
 		<h1>Home</h1>
-		<p>Welcome to your pixel school home</p>
+		
+
+		<h2>Your Lessons</h2>
+		<div className="lesson-list">
+			{lessonList.map((lesson) => (
+				<div key={lesson.slug} className="lesson">
+					<div>{lesson.title}</div>
+
+				</div>
+			))}
+		</div>
+
 	</div>);
 }
