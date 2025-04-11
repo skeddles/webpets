ff69aada554570f516bd91d78cbcad434aa649f5
Sam Keddy <skeddy13@yahoo.com>
2025-04-11 15:27:41 -0400

    fixed homepage full width overflow

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -12,6 +12,10 @@ export default function Home({}: HomeProps) {
 		{ title: 'Lesson 1', slug: 'lesson-1' },
 		{ title: 'Lesson 2', slug: 'lesson-2' },
 		{ title: 'Lesson 3', slug: 'lesson-3' },
+		{ title: 'Lesson 3', slug: 'lesson-3' },
+		{ title: 'Lesson 3', slug: 'lesson-3' },
+		{ title: 'Lesson 3', slug: 'lesson-3' },
+		{ title: 'Lesson 3', slug: 'lesson-3' },
 	];
 
 	return (<div className="Home">
