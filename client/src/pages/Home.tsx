5cd4061eedfd4762917cac6dc4345dc602ac956b
Sam Keddy <skeddy13@yahoo.com>
2025-04-11 15:50:05 -0400

    made home lesson list use links

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router";
 
 import './Home.css';
 
@@ -9,7 +9,7 @@ interface HomeProps {
 export default function Home({}: HomeProps) {
 
 	const lessonList = [
-		{ title: 'Lesson 1', slug: 'lesson-1' },
+		{ title: 'Lesson 1', slug: 'test-lesson' },
 		{ title: 'Lesson 2', slug: 'lesson-2' },
 		{ title: 'Lesson 3', slug: 'lesson-3' },
 		{ title: 'Lesson 3', slug: 'lesson-3' },
@@ -25,10 +25,10 @@ export default function Home({}: HomeProps) {
 		<h2>Your Lessons</h2>
 		<div className="lesson-list">
 			{lessonList.map((lesson) => (
-				<div key={lesson.slug} className="lesson">
+				<Link to={'/lesson/'+lesson.slug} key={lesson.slug} className="lesson" >
 					<div>{lesson.title}</div>
 
-				</div>
+				</Link>
 			))}
 		</div>
 
