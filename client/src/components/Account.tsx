c6649025ba61e57c186834cb096145e0bb7b0669
Sam Keddy <skeddy13@yahoo.com>
2025-04-16 12:22:30 -0400

    account sidebar toggle

--- client/src/components/Account.tsx
+++ client/src/components/Account.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useAppState } from '../hooks/AppState';
 
 import ThemeSelector from './ThemeSelector';
@@ -7,11 +8,24 @@ import '../css/Account.css';
 export default function Account() {
 	const { state: { user } } = useAppState();
 
-	return (<div className="Account">
-		<div>{user.username} </div>
-		<div>({user.admin && <span className="admin">Admin</span>})</div>
-		
-		<ThemeSelector />
+	const [open, setOpen] = useState(false);
+
+	return (<div className={"Account" + (open ? " open" : "")}>
+
+		<div className="user-button">
+
+			<button onClick={() => setOpen(!open)}>
+				{user.username}
+			</button>
+		</div>
+
+		<div className="content">
+
+			<div>({user.admin && <span className="admin">Admin</span>})</div>
+			
+			<ThemeSelector />
+
+		</div>
 
 	</div>);
 }
