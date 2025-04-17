c0682f4d23e978020feec4281f0d7da9fa89544a
Sam Keddy <skeddy13@yahoo.com>
2025-04-17 08:58:36 -0400

    lessons now sorted by number

--- server/src/queries/assignments/get-many-by-lesson-slug.ts
+++ server/src/queries/assignments/get-many-by-lesson-slug.ts
@@ -1,7 +1,7 @@
 import { Assignments } from '../../database.js';
 
 export async function getAssignmentsByLessonSlug (lessonSlug:string) {
-	let assignments = await Assignments.find({lessonSlug}).toArray();
+	let assignments = await Assignments.find({lessonSlug}).sort({ number: 1 }).toArray();
 	if (!assignments) throw new Error("Not Found");
 	return assignments as Assignment[];
 }
