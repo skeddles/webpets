909979f5bd0a97d6d4bce87b06806aaa9774f52f
Sam Keddy <skeddy13@yahoo.com>
2025-04-19 11:40:46 -0400

    split assignments into separate component

---
+++ client/src/components/Assignment.tsx
@@ -0,0 +1,62 @@
+import '../css/Assignment.css';
+
+import Button from './Button';
+import CompletedButton from './CompletedButton';
+import DownloadIcon from '../assets/svg/download.svg?react';
+
+const CDN = import.meta.env.VITE_DO_SPACES_CDN_PATH;
+
+interface AssignmentProps {
+	assignment: Assignment;
+	lessonSlug: string;
+}
+
+export default function Assignment({assignment, lessonSlug}: AssignmentProps) {
+
+
+	const assignment_file_url = CDN + '/assignments/' + assignment.notionId + '.png';
+	const assignment_file_download_name = lessonSlug + '_assignment_' + assignment.number + '_' + assignment.name.toLowerCase().replace(/[^a-zA-Z0-9]/g, '_') + '.png';
+	
+	async function downloadFile(): Promise<void> {
+		try {
+			const response: Response = await fetch(assignment_file_url);
+			if (!response.ok) {
+				throw new Error('Failed to fetch the file');
+			}
+	
+			const blob: Blob = await response.blob();
+			const link: HTMLAnchorElement = document.createElement('a');
+			const objectUrl: string = URL.createObjectURL(blob);
+			link.href = objectUrl;
+			link.download = assignment_file_download_name;
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+			URL.revokeObjectURL(objectUrl);
+		} catch (error: unknown) {
+			console.error('Error downloading the file:', error);
+		}
+	}
+
+	return (
+		<div className="Assignment">
+			<h2>Assignment {assignment.number}: {assignment.name}</h2>
+
+			<div className="worksheet">
+				<h6>Worksheet:</h6>
+				<img src={CDN+`/assignments/${assignment.notionId}.png`}/>
+
+				<Button className="download" size="small" onClick={downloadFile}>
+					Download <DownloadIcon />
+				</Button>
+			</div>
+			
+			<div className="description" dangerouslySetInnerHTML={{__html: assignment.description}}></div>
+
+			<div className="complete-assignment">
+				<p>When you've finished the assignment, mark it as complete!</p>
+				<CompletedButton complete={false} type="assignment" contentId={assignment._id} onSuccess={() => {}}/>
+			</div>
+		</div>
+	);
+}
--- client/src/components/Assignments.tsx
+++ client/src/components/Assignments.tsx
@@ -1,12 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
 import useApiRequest from '../hooks/ApiRequest';
 
-import Button from './Button';
+import Assignment from './Assignment';
 
 import '../css/Assignments.css';
-import DownloadIcon from '../assets/svg/download.svg?react';
-
-const CDN = import.meta.env.VITE_DO_SPACES_CDN_PATH;
 
 interface AssignmentsProps {
 	lessonSlug: string;
@@ -19,7 +16,6 @@ export default function Assignments({lessonSlug}: AssignmentsProps) {
 	const [assignments, setAssignments] = useState<Assignment[] | null>(null);
 
 	async function loadAssignments() {
-		
 		try {
 			const result = await apiRequest('assignments/lesson', { slug: lessonSlug });
 			if (!result) throw new Error('Assignments data is null');
@@ -49,60 +45,9 @@ export default function Assignments({lessonSlug}: AssignmentsProps) {
 		return () => {window.removeEventListener('scroll', handleScroll);};
 	}, []);
 
-	function downloadAssignment(assignmentIndex: number) {
-		const assignment = assignments![assignmentIndex];
-		const url = CDN + '/assignments/' + assignment.notionId + '.png';
-		const filename = lessonSlug + '_assignment_' + assignment.number + '_' + assignment.name.toLowerCase().replace(/[^a-zA-Z0-9]/g, '_') + '.png';
-		downloadFile(url, filename);
-	}
-
-
 	return (<div className="Assignments" ref={AssignmentsRef}>
 		<h1>Assignments</h1>
-		{assignments && assignments.map((assignment, i) => {
-			return (
-				<div key={assignment._id} className="assignment">
-					<h2>Assignment {assignment.number}: {assignment.name}</h2>
-
-					<div className="worksheet">
-
-						<h6>Worksheet:</h6>
-
-						<img src={CDN+`/assignments/${assignment.notionId}.png`}/>
-
-						<Button className="download" size="small" onClick={()=> downloadAssignment(i)}>
-							Download <DownloadIcon />
-						</Button>
-
-					</div>
-					
-					<div className="description" dangerouslySetInnerHTML={{__html: assignment.description}}>
-
-					</div>
-				</div>
-			);
-		})}
+		{assignments && assignments.map((assignment) => <Assignment key={assignment._id} assignment={assignment} lessonSlug={lessonSlug} />)}
 		{!assignments && <p>Loading...</p>}
 	</div>);
-}
-
-async function downloadFile(url: string, filename: string): Promise<void> {
-	try {
-		const response: Response = await fetch(url);
-		if (!response.ok) {
-			throw new Error('Failed to fetch the file');
-		}
-
-		const blob: Blob = await response.blob();
-		const link: HTMLAnchorElement = document.createElement('a');
-		const objectUrl: string = URL.createObjectURL(blob);
-		link.href = objectUrl;
-		link.download = filename;
-		document.body.appendChild(link);
-		link.click();
-		document.body.removeChild(link);
-		URL.revokeObjectURL(objectUrl);
-	} catch (error: unknown) {
-		console.error('Error downloading the file:', error);
-	}
 }
