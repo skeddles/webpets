d01e1ffb1b305afa0490728dfacb1ba2b287416f
Sam Keddy <skeddy13@yahoo.com>
2025-04-28 09:58:04 -0400

    added shop checkout button

--- client/src/components/ShoppingCartPanel.tsx
+++ client/src/components/ShoppingCartPanel.tsx
@@ -1,6 +1,9 @@
 import { useAppState } from '../hooks/AppState';
 
+import Button from './Button';
+
 import '../css/ShoppingCartPanel.css';
+import ChevronRightIcon from '../assets/svg/chevron-right.svg?react';
 
 interface CartProps {
 	open: boolean;
@@ -19,17 +22,26 @@ export default function ShoppingCartPanel({open}: CartProps) {
 
 			{shoppingCart.length === 0 && <p>Your cart is empty</p>}
 
-
-			{shoppingCart.map((product) => (
-				<div key={product.id} className="cart-item">
-					<img src={`https://via.placeholder.com/150?text=${product.id}`} alt={product.id} />
-					<div className="cart-item-details">
-						<h3>{product.id}</h3>
-						<p>Price: ${product.price / 100}</p>
-						<p>Quantity: {product.quantity}</p>
+			{shoppingCart.length > 0 && <>
+			
+				{shoppingCart.map((product) => (
+					<div key={product.id} className="cart-item">
+						<img src={`https://via.placeholder.com/150?text=${product.id}`} alt={product.id} />
+						<div className="cart-item-details">
+							<h3>{product.id}</h3>
+							<p>Price: ${product.price / 100}</p>
+							<p>Quantity: {product.quantity}</p>
+						</div>
 					</div>
-				</div>
-			))}
+				))}
+			
+				<Button>
+					Checkout <ChevronRightIcon />
+				</Button>
+			</>}
+
+
+
 		</div>
 	</div>);
 }
