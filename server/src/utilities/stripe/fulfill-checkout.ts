4ad1104ad602cec7eac86fe6dd93234d8d1c889e
Sam Keddy <skeddy13@yahoo.com>
2025-05-06 13:47:50 -0400

    fulfill-checkout now sends discord webhook on success

--- server/src/utilities/stripe/fulfill-checkout.ts
+++ server/src/utilities/stripe/fulfill-checkout.ts
@@ -36,14 +36,32 @@ export default async function fulfillCheckout(event:StripeCompleteCheckoutEvent)
 
 		await addUserPurchases(userId, purchasedLessonsIds);
 		await insertPurchase(userId, sessionId, purchasedProductPriceIds);
+		console.log('Checkout session fulfilled');
+		sendCompletedWebook(event);
 	} catch (error) {
 		sendDiscordWebhook('ERROR', error);
 		throw error; 
 	}
 }
 
-
-
 function getPriceIdsOfType(lineItems: ExpandedLineItem[], type: string) {
 	return lineItems.filter(li => li.price.product.metadata.type === type).map(li => li.price.id);
+}
+
+function sendCompletedWebook (event:StripeCompleteCheckoutEvent) {
+
+	const data = event.data.object as Stripe.Checkout.Session;
+
+	let text = '# New Purchase:\n\n';
+
+	text += `**User ID:** ${data.client_reference_id}\n`;
+	text += `**Session ID:** ${data.id}\n`;
+	text += `**Payment Status:** ${data.payment_status}\n`;
+
+	if (data.amount_total && data.currency) 
+		text += `**Total Amount:** ${data.amount_total / 100} ${data.currency.toUpperCase()}\n`;
+	else
+		text += `**Total Amount:** N/A\n`;
+
+	sendDiscordWebhook('PURCHASE', text);
 }
