84db221e828eae624592b38afa98a78644b9a4d3
Sam Keddy <skeddy13@yahoo.com>
2025-05-12 14:26:53 -0400

    added loading to router

--- client/src/layouts/LayoutApp.tsx
+++ client/src/layouts/LayoutApp.tsx
@@ -1,12 +1,19 @@
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
+import Loading from '../components/Loading';
 
 const Layout = () => {
+	const navigation = useNavigation();
+	const isNavigating = Boolean(navigation.location);
+
 	return (<div className="App">
 		<Navigation />
-		<div className="tab"><Outlet /></div>
+		<div className="tab">
+			{isNavigating && <Loading />}
+			<Outlet />
+		</div>
 		<Footer />
 	</div>);
 };
